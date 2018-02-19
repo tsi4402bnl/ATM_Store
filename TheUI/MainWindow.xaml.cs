55d03a512d94280dacd1345b3adef4fe3ed1dd5e
Mikus <mikus@gmail.com>
2018-02-19 23:42:27 +0200

    MK-001 : image convertion

--- TheUI/MainWindow.xaml.cs
+++ TheUI/MainWindow.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 
 namespace TheUI
@@ -45,6 +49,53 @@ namespace TheUI
         // Log functions
         private void Clear_Log(object sender, RoutedEventArgs e) { logDatabase.Clear_Log(); }
         public void Log(string msg) { logDatabase.Log(msg); }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".png"; // Default file extension
+            dlg.Filter = "Image files (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png";
+            bool? result = dlg.ShowDialog();
+            if (result == true)
+            {
+                try
+                {
+                    ImageSource source = new BitmapImage(new Uri(dlg.FileName));
+                    int width = 100;
+                    int height = 100;
+
+                    var rect = new Rect(0, 0, width, height);
+                    var group = new DrawingGroup();
+                    RenderOptions.SetBitmapScalingMode(group, BitmapScalingMode.HighQuality);
+                    group.Children.Add(new ImageDrawing(source, rect));
+                    var drawingVisual = new DrawingVisual();
+                    using (var drawingContext = drawingVisual.RenderOpen()) drawingContext.DrawDrawing(group);
+                    var resizedImage = new RenderTargetBitmap( width, height, 96, 96, PixelFormats.Default);
+                    resizedImage.Render(drawingVisual);
+                    
+                    byte[] data;
+                    JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(resizedImage));
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        encoder.Save(ms);
+                        data = ms.ToArray();
+                    }
+                    BitmapImage img = new BitmapImage();
+                    img.BeginInit();
+                    img.StreamSource = new MemoryStream(data);
+                    img.EndInit();
+                    imgPhoto.Source = img;
+
+                }
+                catch (System.IO.FileNotFoundException)
+                {
+                    MessageBox.Show("There was an error opening the bitmap." +
+                        "Please check the path.");
+                }
+                string filename = dlg.FileName;
+            }
+        }
 
 
         /********************************************************* Item Tab Events ********************************************************/
@@ -95,13 +146,13 @@ namespace TheUI
         }
 
 
-        /************************************************** Fetch/Fill DB, called from C++ ************************************************/
+        /*********************************************************** Fetch/Fill DB ********************************************************/
         // Fetch
         public bool IsFbMessagePending() { return fbClient == null ? false : fbClient.IsFbMessagePending(); }
         public FbEventData FetchNextFbMessage() { return fbClient == null ? new FbEventData() : fbClient.FetchNextFbMessage(); }
 
         // Add
-        public void AddProperties(string id,    IItemPropEntryFb item)  {     itemDatabase.AddProperties(id, item); }
+        public void AddProperties(string id,     IItemPropEntryFb item) {     itemDatabase.AddProperties(id, item); }
         public void AddProperties(string id, ICategoryPropEntryFb item) { categoryDatabase.AddProperties(id, item); }
         public void AddProperties(string id, ISupplierPropEntryFb item) { supplierDatabase.AddProperties(id, item); }
 
@@ -109,6 +160,7 @@ namespace TheUI
         public void     RemoveItemProperties(string id) {     itemDatabase.RemoveProperties(id); }
         public void RemoveCategoryProperties(string id) { categoryDatabase.RemoveProperties(id); }
         public void RemoveSupplierProperties(string id) { supplierDatabase.RemoveProperties(id); }
+
     }
 
 
