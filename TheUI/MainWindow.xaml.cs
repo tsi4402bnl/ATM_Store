8a9c8d81c22c0fe04c6d0add6029aea9bb21fb4f
Mikus Kaiva <MikusK1@verifone.com>
2018-02-28 16:46:11 +0200

    Iteration-001 : Created iteration 001

--- TheUI/MainWindow.xaml.cs
+++ TheUI/MainWindow.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Threading;
+﻿using System.Windows;
 
 namespace TheUI
 {
@@ -11,11 +6,8 @@ namespace TheUI
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     /// 
-    public sealed partial class MainWindow : Window, IDisposable
+    public sealed partial class MainWindow : Window
     {
-        // public fields / properties
-        public FireBase fbClient;
-
         // private fields / properties
         private LogDataBase logDatabase;
 
@@ -24,37 +16,20 @@ namespace TheUI
 
             InitializeComponent();
 
-            new FbMessageProcessor(this);
-
             logDatabase = new LogDataBase(Dispatcher);
             DataContext = logDatabase.LogEntries;
 
             RegisterUiControlEvents();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            fbClient = new FireBase(Dispatcher);
-        }
-
         // Log functions
         private void BtnClearLog_Log(object sender, RoutedEventArgs e) { logDatabase.Clear_Log(); }
         public void Log(string msg) { logDatabase.Log(msg); }
-
-        public void Dispose()
-        {
-            fbClient.Dispose();
-        }
 
         private void RegisterUiControlEvents()
         {
             UcLog.BtnClearLog.Click += BtnClearLog_Log;
         }
-
-        /*********************************************************** Fetch/Fill DB ********************************************************/
-        // Fetch
-        public bool IsFbMessagePending() { return fbClient == null ? false : fbClient.IsFbMessagePending(); }
-        public FbEventData FetchNextFbMessage() { return fbClient == null ? new FbEventData() : fbClient.FetchNextFbMessage(); }
 
         
     }
